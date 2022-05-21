8db8984cdc98892088fb468bcb4fca8e12d8feb3
Ilya <itroshchinskiy@rambler.ua>
2022-05-21 20:48:03 +0300

    forth

--- currency_exchange/tests/test_celery_tasks.py
+++ currency_exchange/tests/test_celery_tasks.py
@@ -5,14 +5,14 @@
 from currency_exchange.tasks import download_exchange_rates
 from rest_framework.test import APITestCase
 
-#
-# class TestAddTask(APITestCase):
-#
-#     def test_task_download_exchange_rates(self):
-#         """
-#         Test celery tasks which upload currency rates to db
-#         :return:
-#         """
-#         results = download_exchange_rates.apply()
-#         self.assertEqual(results.get(), 'Task download exchange rates executed successfully')
-#         self.assertEqual(results.state, 'SUCCESS')
+
+class TestAddTask(APITestCase):
+
+    def test_task_download_exchange_rates(self):
+        """
+        Test celery tasks which upload currency rates to db
+        :return:
+        """
+        results = download_exchange_rates.apply()
+        self.assertEqual(results.get(), 'Task download exchange rates executed successfully')
+        self.assertEqual(results.state, 'SUCCESS')
